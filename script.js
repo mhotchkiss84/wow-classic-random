4445e85590fe47dff072f40d30fee0ef21b725a3
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-04-21 19:20:31 -0400

    Finished all logic except for the random spec option. Also added CSS for container

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function randomSpec(){}
 function randomRace(faction){
     const allianceRaces = ["Dwarf", "Gnome", "Human", "Night Elf"]
     const hordeRaces = ["Orc", "Tauren", "Troll", "Undead"]
-    const finalRace = ""
+    let finalRace = ""
     if(faction === "Alliance"){
         finalRace = allianceRaces[Math.floor(Math.random() * allianceRaces.length)];
     } else{
@@ -27,23 +27,57 @@ function randomRace(faction){
 }
 
 //Function for random class
-function randomClass(){
-    const classList = []
-    1;
+function randomClass(race){
+    let finalClass = ""
+    const dwarfClassList = ["Hunter", "Paladin", "Priest", "Rogue", "Warrior"]
+    const gnomeClassList = ["Mage", "Rogue", "Warlock", "Warrior"]
+    const humanClassList = ["Paladin", "Priest", "Mage", "Rogue", "Warlock", "Warrior"]
+    const nightElfClassList = ["Druid", "Hunter", "Priest", "Rogue", "Warrior"]
+    const orcClassList = ["Hunter", "Rogue", "Shaman", "Warlock", "Warrior"]
+    const taurenClassList = ["Druid", "Hunter", "Shaman", "Warrior"]
+    const trollClassList = ["Hunter", "Mage", "Priest", "Rogue", "Shaman", "Warrior"]
+    const undeadClassList = ["Mage", "Priest", "Rogue", "Warlock", "Warrior"]
+    if(race === "Dwarf"){
+        finalClass = dwarfClassList[Math.floor(Math.random() * dwarfClassList.length)];
+    } else if(race === "Gnome"){
+        finalClass = gnomeClassList[Math.floor(Math.random() * gnomeClassList.length)];
+    } else if(race === "Human"){
+        finalClass = humanClassList[Math.floor(Math.random() * humanClassList.length)];
+    } else if(race === "Night Elf"){
+        finalClass = nightElfClassList[Math.floor(Math.random() * nightElfClassList.length)];
+    } else if(race === "Orc"){
+        finalClass = orcClassList[Math.floor(Math.random() * orcClassList.length)];
+    } else if(race === "Tauren"){
+        finalClass = taurenClassList[Math.floor(Math.random() * taurenClassList.length)];
+    } else if(race === "Troll"){
+        finalClass = trollClassList[Math.floor(Math.random() * trollClassList.length)];
+    } else if (race === "Undead"){
+        finalClass = undeadClassList[Math.floor(Math.random() * undeadClassList.length)];
+    }
+    return finalClass
 }
 
 //Event listener for user input
 document.querySelector(`#random-btn`).addEventListener("click", generateEvent => {
     const server = document.querySelector(`#server`).value
     const faction = document.querySelector(`#faction`).value
+    let endFaction
     const gender = document.querySelector(`#gender`).value
+    let endGender = ""
+    let endClass = ""
+    let endRace = ""
     const spec = document.querySelector(`#spec`).value
     if(faction === "yes"){
-        const endFaction = randomFaction()
-        console.log(endFaction);
+        endFaction = randomFaction()
+        // console.log(endFaction);
     }
     if(gender === "yes"){
-        const endGender = randomGender()
-        console.log(endGender);
+        endGender = randomGender()
+        // console.log(endGender);
     }
+    //Gotta do race first then class.....
+    endRace = randomRace(endFaction)
+    endClass = randomClass(endRace)
+    console.log(endFaction, endRace, endGender, endClass);
+    return document.querySelector(`#output-container`).innerHTML = `<p> You are an ${endFaction} ${endRace} ${endGender} ${endClass}`
 })
