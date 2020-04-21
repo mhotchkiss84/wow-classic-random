39fd546c186f06465afd47b876048d62e9002312
Mike Hotchkiss <sxs.linux@hotmail.com>
2020-04-21 17:51:23 -0400

    Created event listener for generate button which creates variables with the user input

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(`#random-btn`).addEventListener("click", generateEvent => {
+    const server = document.querySelector(`#server`).value
+    const faction = document.querySelector(`#faction`).value
+    const gender = document.querySelector(`#gender`).value
+    const spec = document.querySelector(`#spec`).value
+    console.log(spec);
+})
